c7b10191a235300d38be0337ddb56a9b73d5de04
IT19209012 <it19209012@my.sliit.lk>
2022-10-22 19:32:36 +0530

    register page linked

--- app/src/main/java/com/example/fuelapp/Register/RegisterActivity.java
+++ app/src/main/java/com/example/fuelapp/Register/RegisterActivity.java
@@ -2,15 +2,43 @@ package com.example.fuelapp.Register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fuelapp.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+
+    private Button User;
+    private Button Stations;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.fuelapp.R.layout.activity_register);
+
+        User = findViewById(R.id.user);
+        Stations = findViewById(R.id.admin);
+
+        User.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, VehicleRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Stations.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, StationRegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
