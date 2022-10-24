b236d6d207da7902b079a9306d54d95ae1a94656
IT19209012 <it19209012@my.sliit.lk>
2022-10-24 23:46:59 +0530

    Dinisuru

--- app/src/main/java/com/example/fuelapp/Model/User.java
+++ app/src/main/java/com/example/fuelapp/Model/User.java
@@ -1,30 +1,128 @@
 package com.example.fuelapp.Model;
 
+
+import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
+
 public class User {
-    private int userId;
 
-    private int id;
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("role")
+    @Expose
+    private String role;
+    @SerializedName("fueltype")
+    @Expose
+    private String fueltype;
+    @SerializedName("vehicletype")
+    @Expose
+    private String vehicletype;
+    @SerializedName("vehicleno")
+    @Expose
+    private String vehicleno;
+    @SerializedName("city")
+    @Expose
+    private String city;
+    @SerializedName("fuelcompany")
+    @Expose
+    private String fuelcompany;
 
-    private String title;
-
-    @SerializedName("body")
-    private String text;
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public int getId() {
+    public String getId() {
         return id;
     }
 
-    public String getTitle() {
-        return title;
+    public void setId(String id) {
+        this.id = id;
     }
 
-    public String getText() {
-        return text;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getFueltype() {
+        return fueltype;
+    }
+
+    public void setFueltype(String fueltype) {
+        this.fueltype = fueltype;
+    }
+
+    public String getVehicletype() {
+        return vehicletype;
+    }
+
+    public void setVehicletype(String vehicletype) {
+        this.vehicletype = vehicletype;
+    }
+
+    public String getVehicleno() {
+        return vehicleno;
+    }
+
+    public void setVehicleno(String vehicleno) {
+        this.vehicleno = vehicleno;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getFuelcompany() {
+        return fuelcompany;
+    }
+
+    public void setFuelcompany(String fuelcompany) {
+        this.fuelcompany = fuelcompany;
+    }
+
+    public User(String email, String password, String role, String city, String fuelcompany) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.city = city;
+        this.fuelcompany = fuelcompany;
+    }
+
+    public User(String email, String password, String role, String fueltype, String vehicletype, String vehicleno) {
+        this.email = email;
+        this.password = password;
+        this.role = role;
+        this.fueltype = fueltype;
+        this.vehicletype = vehicletype;
+        this.vehicleno = vehicleno;
+        this.city = city;
+        this.fuelcompany = fuelcompany;
     }
 }
--- app/src/main/java/com/example/fuelapp/Register/VehicleRegisterActivity.java
+++ app/src/main/java/com/example/fuelapp/Register/VehicleRegisterActivity.java
@@ -2,15 +2,108 @@ package com.example.fuelapp.Register;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.fuelapp.Interface.IUserAPI;
+import com.example.fuelapp.Model.Controller;
+import com.example.fuelapp.Model.User;
+import com.example.fuelapp.Model.UserLoginResponse;
 import com.example.fuelapp.R;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class VehicleRegisterActivity extends AppCompatActivity {
 
+    private TextView vehicleNo;
+    private Spinner fuelType;
+    private Spinner vehicleType;
+    private TextView email;
+    private TextView password;
+    private Button register;
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.example.fuelapp.R.layout.activity_vehicle_register);
+        
+        vehicleNo = findViewById(R.id.txtvehicle);
+        fuelType = findViewById(R.id.spnFuel);
+        vehicleType = findViewById(R.id.spnVehicle);
+        email = findViewById(R.id.txtEmail);
+        password = findViewById(R.id.txtpassword);
+        register = findViewById(R.id.btnRegister);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onRegister();
+
+            }
+        });
+        
     }
+    
+    protected void onRegister() {
+        String vehicleNo = this.vehicleNo.getText().toString();
+        String fuelType = this.fuelType.getSelectedItem().toString();
+        String vehicleType = this.vehicleType.getSelectedItem().toString();
+        String email = this.email.getText().toString();
+        String password = this.password.getText().toString();
+        
+        if(vehicleNo.isEmpty()){
+            this.vehicleNo.setError("Vehicle No is required");
+            this.vehicleNo.requestFocus();
+            return;
+        }
+        
+        if(email.isEmpty()){
+        this.email.setError("Email is required");
+        this.email.requestFocus();
+        return;
+        }
+
+        if(password.isEmpty() || password.length() < 6){
+        this.password.setError("Password must be at least 6 characters");
+        this.password.requestFocus();
+        return;
+         }
+
+        User user = new User(email, password,"user",fuelType,vehicleType,vehicleNo);
+
+        IUserAPI iUserAPI = Controller.getRetrofit().create(IUserAPI.class);
+        Call<UserLoginResponse> call = iUserAPI.SaveUser(user);
+
+        call.enqueue(new Callback<UserLoginResponse>() {
+            @Override
+            public void onResponse(Call<UserLoginResponse> call, Response<UserLoginResponse> response) {
+                Log.e("Response", "Response code : "+ response.code());
+                if(response.code() == 200){
+                    Log.e("Response", "test : "+response.body().getData().getId());
+                    Log.e("Response", "test2 : "+response.body().getData().getRole());
+
+                    Toast.makeText(VehicleRegisterActivity.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<UserLoginResponse> call, Throwable t) {
+                Toast.makeText(VehicleRegisterActivity.this, "Registration Fail", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        
+        
+    }
+    
 }
